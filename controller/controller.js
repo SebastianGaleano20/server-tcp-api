75c7963440c3bef297fb1c3b2ef4db12dbea2e43
Sebastian Galeano <sebascarp9@gmail.com>
2023-11-15 22:20:59 -0300

    Creacion de modulos/controladores

---
+++ controller/controller.js
@@ -0,0 +1,45 @@
+import { readData, writeData } from "../utils/utils.js";
+
+//getters
+const getPizzas = () => {
+  const pizzas = readData();
+  return pizzas;
+};
+
+const getPizzaById = (id) => {
+  const pizzas = readData();
+  const pizza = pizzas.find((pizza) => {
+    pizza.id = id;
+  });
+  return pizza;
+};
+
+const addPizza = (newPizza) => {
+  const pizzas = readData();
+  const pizza = pizzas.find((pizza) => {
+    pizza.nombre === newPizza.nombre;
+  });
+  if (pizza) {
+    return "Pizza exists, please add new pizza with other name.";
+  }
+  pizzas.push(newPizza);
+  writeData(JSON.stringify(pizzas));
+  return "Pizza added successfully :)";
+};
+
+const deletePizza = (id) => {
+    const pizzas = readData();
+    const pizza = pizzas.find((pizza) =>{
+        pizza.id === id;
+    })
+    if(pizza){
+        const newPizzas = pizza.filter((pizza)=>pizza.id !== id);
+        writeData(JSON.stringify(newPizzas));
+        return "Pizza deleted successfully";
+    }
+    return "Pizza does not exist";
+};
+
+console.log(getPizzas());
+
+export { getPizzas, getPizzaById, addPizza, deletePizza };
