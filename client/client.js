3a8a6a87b8aeb1001ce39bb62b7a85ee3e64e432
Sebastian Galeano <sebascarp9@gmail.com>
2023-11-15 22:03:08 -0300

    Terminacion de Client

--- client/client.js
+++ client/client.js
@@ -5,14 +5,19 @@ dotenv.config();
 const options = {
     port: process.env.PORT,
     host: process.env.HOST
-}
+};
 
 const clientTCP = net.createConnection(options);
 
 clientTCP.on("connect", ()=>{
     console.log("conecction successful");
-    clientTCP.end();
-})
+    const request = process.argv.splice(2);
+    clientTCP.write(JSON.stringify(request));
+});
+
 clientTCP.on("data",()=>{
-    console.log("Answer of server");
-})
+    console.log("Answer of server: ");
+    const data = JSON.parse(jsonData.toString());
+    console.log(data);
+    clientTCP.end();
+});
